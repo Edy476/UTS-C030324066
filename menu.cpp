6d40fe22224f89d7f2bae0482d2f1d676aea0419
Edy476 <loclactt@gmail.com>
2025-05-27 08:28:59 +0800

    memperbaiki kelemahan menu input dan output

--- menu.cpp
+++ menu.cpp
@@ -13,47 +13,71 @@ void dMenu(){
     cout << "Masukan angka :";        
 }
 
-void input(int data[]){
+void input(int data[], int &n){
     system("cls");
-    
-    // Memasukkan data
-    cout << "Masukkan 5 angka: ";
-    for (int i = 0; i < 5; i++) {
-        cin >> data[i];
+
+    cout << "Berapa banyak angka yang ingin Anda masukkan (maks 100)? ";
+    cin >> n;
+
+    if (n < 1 || n > 100) {
+        cout << "Jumlah tidak valid. Angka harus antara 1-100.";
+        n = 0; // Reset n
+    } else {
+        for (int i = 0; i < n; i++) {
+            cout << "Masukkan angka ke-" << (i + 1) << ": ";
+            cin >> data[i];
+        }
     }
-    getch();
-}
-
-void output(int data[]){
-    system("cls");
-
-    // Menampilkan data
-    cout << "\nData yang Anda masukkan adalah:\n";
-    for (int i = 0; i < 5; i++)
+   
+    if (n < 1 || n > 100)
     {
-        cout << "Angka ke-" << (i + 1) << ": " << data[i] << endl;
+        cout << endl << "maaf tidak bisa memasukkan data :(" ;
+    } else{
+
+        cout << "Data anda telah disimpan ^_^";
     }
+    
     getch();
 }
 
-void sorting(int data[]) {
+void output(int data[], int n){
     system("cls");
-    
-    // Exchange Sort
-    for (int i = 0; i < 5 - 1; i++) {
-        for (int j = i + 1; j < 5; j++) {
-            if (data[i] > data[j]) {
-                int temp = data[i];
-                data[i] = data[j];
-                data[j] = temp;
-            }
+
+    if (n == 0) {
+        cout << "Belum ada data yang dimasukkan.\n";
+    } else {
+        cout << "\nData yang Anda masukkan adalah:\n";
+        for (int i = 0; i < n; i++) {
+            cout << "Angka ke-" << (i + 1) << ": " << data[i] << endl;
         }
     }
 
-    cout << "Data setelah diurutkan (dari kecil ke besar):\n";
-    for (int i = 0; i < 5; i++) {
-        cout << "Angka ke-" << (i + 1) << ": " << data[i] << endl;
+    getch();
+}
+
+void sorting(int data[], int n) {
+    system("cls");
+
+    if (n == 0) {
+        cout << "Belum ada data yang dimasukkan.\n";
+    } else {
+        // Exchange Sort
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (data[i] > data[j]) {
+                    int tukar = data[i];
+                    data[i] = data[j];
+                    data[j] = tukar;
+                }
+            }
+        }
+        //output hasil data yg disorting
+        cout << "Data setelah diurutkan (dari kecil ke besar):\n";
+        for (int i = 0; i < n; i++) {
+            cout << "Angka ke-" << (i + 1) << ": " << data[i] << endl;
+        }
     }
+
     getch();
 }
 
@@ -64,26 +88,29 @@ void sepatahKata(){
 }
 
 int main() {
-    int data[5];
+    int data[100];
+    int n = 0; // jumlah data aktif
     char pl;
+
     do {
         dMenu();
         pl = getch();
         switch (pl)
         {
         case '1':
-            input(data);
+            input(data, n);
             break;
         case '2':
-            output(data);
+            output(data, n);
             break;  
         case '3':
-            sorting(data);
+            sorting(data, n);
             break;  
         case '4':
             sepatahKata();
             break;  
         case '5':
+        cout << endl << "anda telah keluar dari menu";
             break;
         default:
             system("cls");
